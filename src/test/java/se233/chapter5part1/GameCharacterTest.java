95bb308b6ae3f07a2d9afb293acb8471306d65e9
natthapoomsaengkaew <natthapoomsaengkaew@gmail.com>
2025-09-24 21:01:10 +0700

    remove comment

--- src/test/java/se233/chapter5part1/GameCharacterTest.java
+++ src/test/java/se233/chapter5part1/GameCharacterTest.java
@@ -80,106 +80,106 @@ public class GameCharacterTest {
         assertTrue(yAcceleration1 == yAcceleration2, "Acceleration is not change");
     }
 
-//    @Test
-//    public void whenTryToGoThroughLeftWall_thenReachLeftWall(){
-//        gameCharacter.respawn();
-//        gameCharacter.setX(0);
-//        gameCharacter.moveLeft();
-//        gameCharacter.moveX();
-//        gameCharacter.checkReachGameWall();
-//
-//        assertEquals(0, gameCharacter.getX(),"Not equal to 0");
-//    }
+    @Test
+    public void whenTryToGoThroughLeftWall_thenReachLeftWall(){
+        gameCharacter.respawn();
+        gameCharacter.setX(0);
+        gameCharacter.moveLeft();
+        gameCharacter.moveX();
+        gameCharacter.checkReachGameWall();
 
-//    @Test
-//    public void whenTryToGoThroughRightWall_thenReachRightWall(){
-//        gameCharacter.respawn();
-//        gameCharacter.setX(GameStage.WIDTH);
-//        gameCharacter.moveRight();
-//        gameCharacter.moveX();
-//        gameCharacter.checkReachGameWall();
-////        System.out.println(GameStage.WIDTH);
-////        System.out.println(gameCharacter.getCharacterWidth());
-////        System.out.println(gameCharacter.getX());
-//        assertEquals(GameStage.WIDTH- gameCharacter.getCharacterWidth(),gameCharacter.getX(),"Not at right wall");
-//    }
+        assertEquals(0, gameCharacter.getX(),"Not equal to 0");
+    }
 
-//    @Test
-//    public void whenNotJumping_canJump() throws IllegalAccessException {
-//        gameCharacter.respawn();
-//        canJumpField.setBoolean(gameCharacter,true);
-////        isJumpingField.setBoolean(gameCharacter, false);
-//        int initialY=gameCharacter.getY();
-//        gameCharacter.jump();
-//        gameCharacter.moveY();
-//        assertTrue(gameCharacter.getY() < initialY, "Jumping is not possible");
-//    }
+    @Test
+    public void whenTryToGoThroughRightWall_thenReachRightWall(){
+        gameCharacter.respawn();
+        gameCharacter.setX(GameStage.WIDTH);
+        gameCharacter.moveRight();
+        gameCharacter.moveX();
+        gameCharacter.checkReachGameWall();
+        System.out.println(GameStage.WIDTH);
+        System.out.println(gameCharacter.getCharacterWidth());
+        System.out.println(gameCharacter.getX());
+        assertEquals(GameStage.WIDTH- gameCharacter.getCharacterWidth(),gameCharacter.getX(),"Not at right wall");
+    }
 
-//    @Test
-//    public void whenJumpingAndFalling_cannotJump() throws IllegalAccessException {
-//        gameCharacter.respawn();
-//        int initialY=gameCharacter.getY();
-//        isJumpingField.set(gameCharacter,false);
-//        isFallingField.set(gameCharacter,true);
-//        gameCharacter.jump(); //jump while falling
-//        gameCharacter.moveY();
-//        assertTrue(gameCharacter.getY() > initialY, "Current Y must be below of initY  when falling.");
-////        assertTrue(yVelocityField.getInt(gameCharacter)>0,"yVelocity should be greater than 0 while falling");
-//    }
+    @Test
+    public void whenNotJumping_canJump() throws IllegalAccessException {
+        gameCharacter.respawn();
+        canJumpField.setBoolean(gameCharacter,true);
+        isJumpingField.setBoolean(gameCharacter, false);
+        int initialY=gameCharacter.getY();
+        gameCharacter.jump();
+        gameCharacter.moveY();
+        assertTrue(gameCharacter.getY() < initialY, "Jumping is not possible");
+    }
 
-//    @Test
-//    public void whenMovingX_collideAndStop(){
-//        gameCharacter.respawn();
-//        GameCharacter mockGameCharacter= Mockito.mock(GameCharacter.class);
-//        mockGameCharacter=new GameCharacter(0, 141, 30, "assets/Character1.png",
-//                4, 3, 2, 111, 97, KeyCode.A, KeyCode.D, KeyCode.W);
-//
-//        gameCharacter.moveRight();
-//        gameCharacter.moveX();
-//        gameCharacter.moveX();
-//        gameCharacter.moveX();
-////        System.out.println(gameCharacter.getX());
-//        gameCharacter.collided(mockGameCharacter);
-////        System.out.println(gameCharacter.getX());
-//
-//        assertEquals(gameCharacter.getX()+gameCharacter.getCharacterWidth(), mockGameCharacter.getX(), "characters overlapped");
-//    }
+    @Test
+    public void whenJumpingAndFalling_cannotJump() throws IllegalAccessException {
+        gameCharacter.respawn();
+        int initialY=gameCharacter.getY();
+        isJumpingField.set(gameCharacter,false);
+        isFallingField.set(gameCharacter,true);
+        gameCharacter.jump(); //jump while falling
+        gameCharacter.moveY();
+        assertTrue(gameCharacter.getY() > initialY, "Current Y must be below of initY  when falling.");
+        assertTrue(yVelocityField.getInt(gameCharacter)>0,"yVelocity should be greater than 0 while falling");
+    }
 
-//    @Test
-//    public void whenFallingX_collideAndStop() throws IllegalAccessException {
-//        gameCharacter.respawn();
-//        GameCharacter mockGameCharacter= Mockito.mock(GameCharacter.class);
-//        mockGameCharacter=new GameCharacter(0, 30, gameCharacter.getY()+gameCharacter.getCharacterHeight(), "assets/Character1.png",
-//                4, 3, 2, 111, 97, KeyCode.A, KeyCode.D, KeyCode.W);
-//
-//        isFallingField.set(gameCharacter,true);
-//        gameCharacter.moveY();
-//
-//        System.out.println(gameCharacter.getY());
-//        gameCharacter.collided(mockGameCharacter);
-//        System.out.println(gameCharacter.getY());
-//
-//        assertTrue(gameCharacter.getY() <= GameStage.GROUND-gameCharacter.getCharacterHeight(), "s");
-//    }
+    @Test
+    public void whenMovingX_collideAndStop(){
+        gameCharacter.respawn();
+        GameCharacter mockGameCharacter= Mockito.mock(GameCharacter.class);
+        mockGameCharacter=new GameCharacter(0, 141, 30, "assets/Character1.png",
+                4, 3, 2, 111, 97, KeyCode.A, KeyCode.D, KeyCode.W);
 
-//    @Test
-//    public void testingSingleKeyPress(){
-//        Keys keys = new Keys();
-//        keys.add(KeyCode.D);
-//        assertTrue(keys.isPressed(KeyCode.D), "D should be detected as pressed");
-//        assertFalse(keys.isPressed(KeyCode.A), "A should not be detected as pressed");
-//    }
-//
-//    @Test
-//    public void testingSequentialKeyPress(){
-//        Keys keys = new Keys();
-//        keys.add(KeyCode.A);
-//        assertTrue(keys.isPressed(KeyCode.A), "A should be detected as pressed");
-//
-//        keys.add(KeyCode.D);
-//        assertTrue(keys.isPressed(KeyCode.D), "D should be detected as pressed");
-//        assertTrue(keys.isPressed(KeyCode.A), "A should be detected as pressed");
-//    }
+        gameCharacter.moveRight();
+        gameCharacter.moveX();
+        gameCharacter.moveX();
+        gameCharacter.moveX();
+        System.out.println(gameCharacter.getX());
+        gameCharacter.collided(mockGameCharacter);
+        System.out.println(gameCharacter.getX());
+
+        assertEquals(gameCharacter.getX()+gameCharacter.getCharacterWidth(), mockGameCharacter.getX(), "characters overlapped");
+    }
+
+    @Test
+    public void whenFallingX_collideAndStop() throws IllegalAccessException {
+        gameCharacter.respawn();
+        GameCharacter mockGameCharacter= Mockito.mock(GameCharacter.class);
+        mockGameCharacter=new GameCharacter(0, 30, gameCharacter.getY()+gameCharacter.getCharacterHeight(), "assets/Character1.png",
+                4, 3, 2, 111, 97, KeyCode.A, KeyCode.D, KeyCode.W);
+
+        isFallingField.set(gameCharacter,true);
+        gameCharacter.moveY();
+
+        System.out.println(gameCharacter.getY());
+        gameCharacter.collided(mockGameCharacter);
+        System.out.println(gameCharacter.getY());
+
+        assertTrue(gameCharacter.getY() <= GameStage.GROUND-gameCharacter.getCharacterHeight(), "s");
+    }
+
+    @Test
+    public void testingSingleKeyPress(){
+        Keys keys = new Keys();
+        keys.add(KeyCode.D);
+        assertTrue(keys.isPressed(KeyCode.D), "D should be detected as pressed");
+        assertFalse(keys.isPressed(KeyCode.A), "A should not be detected as pressed");
+    }
+
+    @Test
+    public void testingSequentialKeyPress(){
+        Keys keys = new Keys();
+        keys.add(KeyCode.A);
+        assertTrue(keys.isPressed(KeyCode.A), "A should be detected as pressed");
+
+        keys.add(KeyCode.D);
+        assertTrue(keys.isPressed(KeyCode.D), "D should be detected as pressed");
+        assertTrue(keys.isPressed(KeyCode.A), "A should be detected as pressed");
+    }
 
 }
 
